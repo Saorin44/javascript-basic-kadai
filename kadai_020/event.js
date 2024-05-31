729407d535d111d1fd5d766a63c81fcad79417fd
Saorin44 <164883984+Saorin44@users.noreply.github.com>
2024-05-31 21:17:18 +0900

    再提出１回目

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -4,7 +4,7 @@ const onBtn = document.getElementById('btn')
 // textというidを持つHTML要素を取得し、定数に代入する
 const chaText = document.getElementById('text')
 
-btn.addEventListener('click', () => {
+onBtn.addEventListener('click', () => {
 // textというidを持つHTML要素の文字を変更
   chaText.textContent="ボタンをクリックしました";
 });
